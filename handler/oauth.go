1263ce76c95fd2a45f78897ad5aac7109c2bc580
Hou Junjie <houjunjie@qiniu.com>
2019-04-19 15:54:03 +0800

    logger for oauth handler

--- handler/oauth.go
+++ handler/oauth.go
@@ -24,28 +24,58 @@ type OAuthRule struct {
 }
 
 func (or *OAuthRule) Verify(w http.ResponseWriter, r *http.Request) {
+	l := or.log.WithValues("host", r.Host, "path", r.URL.Path, "method", r.Method)
 	ck, e := r.Cookie(tokenCookieKey)
 	if e != nil {
+		l.V(4).Info("request without cookie, redirect to login")
 		or.redirectToLogin(w, r)
 		return
 	}
 	token, err := unmarshalToken(ck.Value)
 	if err != nil {
+		l.V(4).Info("invalid token in cookie, redirect to login")
 		or.redirectToLogin(w, r)
 		return
 	}
 	newToken, err := or.Config.TokenSource(context.TODO(), token).Token()
 	if err != nil {
+		l.Error(err, "token rotation failed, redirect to login")
 		or.redirectToLogin(w, r)
 		return
 	}
 	if token.AccessToken != newToken.AccessToken {
+		l.V(4).Info("token refreshed, reload")
 		or.setToken(w, r, newToken)
 		return
 	}
 
 	// valid request shall pass
+	l.V(3).Info("valid request")
 	w.WriteHeader(http.StatusOK)
+}
+
+func (or *OAuthRule) Callback(w http.ResponseWriter, r *http.Request) {
+	l := or.log.WithValues("host", r.Host, "path", r.URL.Path, "method", r.Method)
+	queryState := r.URL.Query().Get("state")
+	ck, e := r.Cookie(stateCookieKey)
+	if e != nil {
+		l.V(3).Info("no state in callback, reject")
+		unauthorized(w, "no state in cookie")
+	}
+	if queryState != ck.Value {
+		l.V(2).Info("state mismatch, reject")
+		unauthorized(w, "state mismatch")
+		return
+	}
+
+	token, err := or.Config.Exchange(context.TODO(), r.URL.Query().Get("code"))
+	if err != nil {
+		l.Error(err, "exchange for")
+		unauthorized(w, "got no token")
+		return
+	}
+
+	or.setToken(w, r, token)
 }
 
 func (or *OAuthRule) redirectToLogin(w http.ResponseWriter, r *http.Request) {
@@ -63,7 +93,7 @@ func (or *OAuthRule) redirectToLogin(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, redirection, http.StatusFound)
 }
 
-// set token into cookie, and retry the original request
+// set token into cookie, and reload the original request
 func (or *OAuthRule) setToken(w http.ResponseWriter, r *http.Request, t *oauth2.Token) {
 	tk, e := marshalToken(t)
 	if e != nil {
@@ -80,24 +110,4 @@ func (or *OAuthRule) setToken(w http.ResponseWriter, r *http.Request, t *oauth2.
 		HttpOnly: true,
 	})
 	http.Redirect(w, r, r.URL.String(), http.StatusFound)
-}
-
-func (or *OAuthRule) Callback(w http.ResponseWriter, r *http.Request) {
-	queryState := r.URL.Query().Get("state")
-	ck, e := r.Cookie(stateCookieKey)
-	if e != nil {
-		unauthorized(w, "no state in cookie")
-	}
-	if queryState != ck.Value {
-		unauthorized(w, "state mismatch")
-		return
-	}
-
-	token, err := or.Config.Exchange(context.TODO(), r.URL.Query().Get("code"))
-	if err != nil {
-		unauthorized(w, "got no token")
-		return
-	}
-
-	or.setToken(w, r, token)
 }
